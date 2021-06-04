ff42432ba0b7c2fb83e3eedb01a1aa345d556d2c
jeroen.admiraal@alliander.com <jeroen.admiraal@alliander.com>
2021-06-04 15:28:57 +0200

    First commit

---
+++ src/main/java/Jeroen/fruitsandnutsapi/FruitsAndNutsApiApplication.java
@@ -0,0 +1,13 @@
+package Jeroen.fruitsandnutsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitsAndNutsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruitsAndNutsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Jeroen/fruitsandnutsapi/FruitsAndNutsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package Jeroen.fruitsandnutsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitsAndNutsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
