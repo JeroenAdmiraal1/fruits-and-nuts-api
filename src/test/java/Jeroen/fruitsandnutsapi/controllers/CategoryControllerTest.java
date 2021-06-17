42d1ec26334de7f340d4a51f97ab314f2d09485a
jeroen.admiraal@alliander.com <jeroen.admiraal@alliander.com>
2021-06-17 10:02:23 +0200

    Add Post and Put to categorycontroller

--- src/test/java/Jeroen/fruitsandnutsapi/controllers/CategoryControllerTest.java
+++ src/test/java/Jeroen/fruitsandnutsapi/controllers/CategoryControllerTest.java
@@ -9,10 +9,12 @@ import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.reactivestreams.Publisher;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 
 @ExtendWith(MockitoExtension.class)
@@ -52,5 +54,33 @@ class CategoryControllerTest {
 		webTestClient.get().uri("/categories/1").exchange()
 				.expectStatus().isOk()
 				.expectBody(Category.class);
+	}
+
+	@Test
+	void testCreateCategories() {
+		BDDMockito.given(categoryRepository.saveAll(any(Publisher.class)))
+				.willReturn(Flux.just(Category.builder().build()));
+
+		Mono<Category> categoryToPost = Mono.just(Category.builder().description("Fruit").build());
+
+		webTestClient.post()
+				.uri("/categories")
+				.body(categoryToPost, Category.class).exchange()
+				.expectStatus().isCreated();
+
+	}
+
+	@Test
+	void testUpdateCategory() {
+		BDDMockito.given(categoryRepository.save(any(Category.class)))
+				.willReturn(Mono.just(Category.builder().build()));
+
+		Mono<Category> categoryToUpdate = Mono.just(Category.builder().description("Fruit").build());
+
+		webTestClient.put()
+				.uri("/categories/1")
+				.body(categoryToUpdate, Category.class).exchange()
+				.expectStatus().isOk();
+
 	}
 }
