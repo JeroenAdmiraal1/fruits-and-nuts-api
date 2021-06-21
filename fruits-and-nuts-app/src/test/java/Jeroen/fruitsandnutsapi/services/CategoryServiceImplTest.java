68a190f0fbe99f07774cf403eb36d4454bcc522f
jeroen.admiraal@alliander.com <jeroen.admiraal@alliander.com>
2021-06-21 11:18:59 +0200

    Make app into multi-module maven built

--- fruits-and-nuts-app/src/test/java/Jeroen/fruitsandnutsapi/services/CategoryServiceImplTest.java
+++ fruits-and-nuts-app/src/test/java/Jeroen/fruitsandnutsapi/services/CategoryServiceImplTest.java
@@ -15,7 +15,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyString;
 
 @ExtendWith(MockitoExtension.class)
--- fruits-and-nuts-app/target/generated-sources/annotations/Jeroen/fruitsandnutsapi/apimodel/mappers/VendorMapperImpl.java
+++ fruits-and-nuts-app/target/generated-sources/annotations/Jeroen/fruitsandnutsapi/apimodel/mappers/VendorMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-06-17T14:17:14+0200",
+    date = "2021-06-21T11:16:53+0200",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.10 (AdoptOpenJDK)"
 )
 @Component
