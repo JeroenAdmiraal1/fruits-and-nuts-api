8acf5e7c21ae4f7e72e81280298f5cb75105d394
jeroen.admiraal@alliander.com <jeroen.admiraal@alliander.com>
2021-06-21 13:11:52 +0200

    xsd document could not be found. Failed to generate classes.

--- fruits-and-nuts-app/src/test/java/Jeroen/fruitsandnutsapi/controllers/AbstractRestControllerTest.java
+++ fruits-and-nuts-app/src/test/java/Jeroen/fruitsandnutsapi/controllers/AbstractRestControllerTest.java
@@ -1,6 +1,5 @@
 package Jeroen.fruitsandnutsapi.controllers;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class AbstractRestControllerTest {
